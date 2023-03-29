3f7e3d5f8c864889b4a615b106f807e3220171a5
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-03-29 19:04:55 +0100

    Create Header and bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 function App() {
 
   return (
-    <div className="App">
+    <div className="bg-teal-600 min-h-screen flex flex-col justify-start items-stretch">
+      <header className='p-5 text-white underline underline-offset-4 bg-teal-900'>
+        <p className='text-white text-5xl font-bold text-center'>Tailwind DewDrop</p>
+      </header>
     </div>
   )
 }
