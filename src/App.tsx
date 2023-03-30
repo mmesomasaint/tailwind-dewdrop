9c675a45ff2f1718e1c47928f47e2a04df39e309
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-03-30 04:26:25 +0100

    add the reflection dot

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <p className='text-white text-5xl font-bold text-center'>Tailwind DewDrop</p>
       </header>
       <main className='flex-grow flex justify-center items-center'>
-        <div className="before:content-[''] before:absolute before:z-10 before:h-64 before:w-64 before:bg-teal-600 before:bg-opacity-40 before:rounded-[3rem] before:rounded-tl-[1rem] before:rounded-br-[15rem] w-64 h-64 bg-gray-300 bg-opacity-80 transform rotate-45 rounded-[13rem] rounded-tl-[2rem] rounded-br-[16rem] overflow-hidden" />
+        <div className="w-64 h-64 bg-gray-300 bg-opacity-90 transform rotate-45 rounded-[13rem] rounded-tl-[2rem] rounded-br-[16rem] overflow-hidden before:content-[''] before:absolute before:z-10 before:h-64 before:w-64 before:bg-teal-600 before:bg-opacity-40 before:rounded-[1rem] before:rounded-tl-[1rem] before:rounded-br-[15rem] after:content-[''] after:absolute after:z-20 after:bg-red-500 after:w-7 after:h-3 after:top-16 after:right-32 after:rounded-3xl after:bg-gray-300" />
       </main>
     </div>
   )
