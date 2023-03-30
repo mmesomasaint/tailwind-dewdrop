ff3d52d14f87a63041d8f8e2cab13708ec568869
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-03-30 05:42:40 +0100

    add dropy animation

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <p className='text-white text-5xl font-bold text-center'>Tailwind DewDrop</p>
       </header>
       <main className='flex-grow flex justify-center items-center'>
-        <div className="w-64 h-64 bg-gray-300 bg-opacity-90 transform rotate-45 rounded-[13rem] rounded-tl-[2rem] rounded-br-[16rem] overflow-hidden before:content-[''] before:absolute before:z-10 before:h-64 before:w-64 before:bg-teal-600 before:bg-opacity-40 before:rounded-[1rem] before:rounded-tl-[1rem] before:rounded-br-[15rem] after:content-[''] after:absolute after:z-20 after:bg-red-500 after:w-7 after:h-3 after:top-16 after:right-32 after:rounded-3xl after:bg-gray-300" />
+        <div className="w-64 h-64 bg-gray-300 bg-opacity-90 transform rotate-45 rounded-[13rem] rounded-tl-[2rem] rounded-br-[16rem] overflow-hidden animate-waving-wind-out before:content-[''] before:absolute before:z-10 before:h-64 before:w-64 before:bg-teal-600 before:bg-opacity-40 before:rounded-[1rem] before:rounded-tl-[1rem] before:rounded-br-[15rem] before:animate-waving-wind-in after:content-[''] after:absolute after:z-20 after:w-7 after:h-3 after:top-16 after:right-32 after:rounded-full after:bg-gray-300" />
       </main>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,48 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        waveOut: {
+          '0%': {
+            'border-radius': '13rem',
+            'border-top-left-radius': '2rem',
+            'border-bottom-right-radius': '16rem'
+          },
+          '50%': {
+            'border-radius': '12rem',
+            'border-top-left-radius': '2rem',
+            'border-bottom-right-radius': '17rem'
+          },
+          '100%': {
+            'border-radius': '13rem',
+            'border-top-left-radius': '2rem',
+            'border-bottom-right-radius': '16rem'
+          },
+        },
+        waveIn: {
+          '0%': {
+            'border-radius': '1rem',
+            'border-top-left-radius': '1rem',
+            'border-bottom-right-radius': '15rem'
+          },
+          '50%': {
+            'border-radius': '3rem',
+            'border-top-left-radius': '1rem',
+            'border-bottom-right-radius': '27rem'
+          },
+          '100%': {
+            'border-radius': '1rem',
+            'border-top-left-radius': '1rem',
+            'border-bottom-right-radius': '15rem'
+          },
+        }
+      },
+      animation: {
+        'waving-wind-out': 'waveOut 2s linear infinite',
+        'waving-wind-in': 'waveIn 2s linear infinite'
+      },
+    },
   },
   plugins: [],
 }
